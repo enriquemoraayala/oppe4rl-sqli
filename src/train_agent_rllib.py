c5b7fd93dbe1a769475bf3df298ffcd984e20d40
Enrique Mora <enrique.mora@es.nestle.com>
2024-01-15 09:25:45 +0100

    fixing ppo

--- src/train_agent_rllib.py
+++ src/train_agent_rllib.py
@@ -6,27 +6,28 @@ from azureml.core import Run
 import ray.rllib.agents.ppo as ppo
 from ray import tune
 from ray.rllib.agents.ppo import PPOTrainer
-#from ray.rllib.algorithms.dqn.dqn import DQNTrainer
+# from ray.rllib.algorithms.dqn.dqn import DQNTrainer
 import ray
 from ray.tune.registry import register_env
 from ray.tune.logger import pretty_print
-import tensorflow as tf
+# import tensorflow as tf
 from gym_waf.envs.waf_brain_env import WafBrainEnv
 
 #tf.compat.v1.disable_eager_execution()
 # number of steps in an episode
 
+
 def main(args):
     # tf.compat.v1.enable_eager_execution()
     ray.init(local_mode=args.local_mode)
-    run = Run.get_context(allow_offline=True)
+    # run = Run.get_context(allow_offline=True)
 
     env_config_fpath = args.env_config
-    #agent_config_fpath = args.agent_config
+    # agent_config_fpath = args.agent_config
     # Read configurations
     with open(env_config_fpath, "rb") as f:
         env_config = json.load(f)
-    #with open(agent_config_fpath, "rb") as f:
+    # with open(agent_config_fpath, "rb") as f:
     #    agent_config = json.load(f)
 
     payloads_fpath = os.path.join(os.path.dirname(
@@ -49,10 +50,10 @@ def main(args):
     env = env_creator(env_config)
     action_space_size = env.action_space.n
     state_space_size = env.observation_space.shape
-    run.tag("actionSpaceSize", action_space_size)
-    run.tag("envStateSpaceSize", state_space_size)
-    for k, v in env_config.items():
-        run.tag(k, v)
+    # run.tag("actionSpaceSize", action_space_size)
+    # run.tag("envStateSpaceSize", state_space_size)
+    # for k, v in env_config.items():
+    #    run.tag(k, v)
 
     scores = []
     agent = "PPO"
@@ -76,7 +77,7 @@ def main(args):
         result = trainer.train()
         if n%5 == 0:
             print(pretty_print(result))
-            file_name = trainer.save('ckpt_ppo_agent_torch_v2')
+            file_name = trainer.save('ckpt_ppo_agent_torch_v3')
         s = "{:3d} reward {:6.2f}/{:6.2f}/{:6.2f} len {:6.2f} saved {}"
         lengths += result["hist_stats"]["episode_lengths"]
         print(s.format(
@@ -89,14 +90,19 @@ def main(args):
         ))
     print(0)
     print(pretty_print(result))
-        
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Train RL agent on WAF-Brain Environment with RLLib")
-    parser.add_argument("--env-config", type=str, help="Path to configuration file of the envionment.",
-                        default="/home/azureuser/cloudfiles/code/Users/Enrique.Mora/rl4pentest/src/config/env.json")
-    parser.add_argument("--local-mode", action="store_true", help="Init Ray in local mode for easier debugging.")
+    parser.add_argument("--env-config", type=str,
+                        help="Path to configuration file of the envionment.",
+                        # default="/home/azureuser/cloudfiles/code/Users/Enrique.Mora/rl4pentest/src/config/env.json")
+                        default='/Users/ESMoraEn/repositories/oppe4rl-sqli/'
+                        + 'src/config/env.json'
+                        )
+    parser.add_argument("--local-mode", action="store_true",
+                        help="Init Ray in local mode for easier debugging.")
     args = parser.parse_args()
     print(f"Running with following CLI options: {args}")
     main(args)
